12db3e375ec8d2c568b34eed26fe70b8e13cd9af
Darren <you@example.comdarren@kandekore.net>
2023-03-30 18:54:27 +0100

    server edit for heoku

--- server/server.js
+++ server/server.js
@@ -41,3 +41,13 @@ const startApolloServer = async (typeDefs, resolvers) => {
   
 // Call the async function to start the server
   startApolloServer(typeDefs, resolvers);
+
+  // if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
+
+db.once("open", () => {
+  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+});
+
